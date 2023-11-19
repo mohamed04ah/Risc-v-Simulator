07af870cc3c6972e78cc8afaf51c3a141411a3da
mohamed04ah <mohamedtarek2004@aucegypt.edu>
2023-11-19 22:15:09 +0200

    .

--- Risc-v-Simulator/main..cpp
+++ Risc-v-Simulator/main..cpp
@@ -1,53 +1,8 @@
 
 #include"Ls_instructions.h"
-int Bintoint1(string value) {
 
-	/*int result = 0;
-
-	for (int i = value.size(); i >= 0; i--) {
-		if (value[i] == '1') {
-
-			result += pow(2, value.size() - i - 1);
-
-		}
-	}
-	return result;*/
-	if (value[0] == '1')
-	{
-		int r1;
-		r1 = bitset<32>(value).to_ullong();
-
-		r1 = -((1 << value.length()) - r1);
-		return r1 + 1;
-	}
-	else
-	{
-		int result = 0;
-		for (int i = value.size(); i >= 0; i--)
-		{
-			if (value[i] == '1') {
-
-				result += pow(2, value.size() - i - 1);
-
-			}
-		}
-		return result;
-	}
-
-	return 0;
-
-
-}
 int main() {
 
 	Risc_v_Simulator r;
-	LS_instructions x; 
-	
-	
-	 
-//cout<<	 Bintoint1(bitset<32>(0).to_string());
 
-	
-
-	
 }
