3c75b6c02875664d0a2acf1dfd7fb27913bad68f
mohamed04ah <mohamedtarek2004@aucegypt.edu>
2023-11-19 21:56:31 +0200

    .

--- Risc-v-Simulator/Memory.cpp
+++ Risc-v-Simulator/Memory.cpp
@@ -1,12 +1,46 @@
 #include "Memory.h"
-
+#include<sstream>
+#include<iomanip>
 Memory& Memory::getinstance()
 {
 	static Memory Mem; 
 	
 	return Mem;
 } 
+string decimal_hexa1(string value)
+{
+	int n = stoi(value);
+	stringstream hex_val;
+	hex_val << hex << n;
+	return hex_val.str();
+}
+int bin_decimal(string value)
+{
 
+	if (value[0] == '1')
+	{
+		int r1;
+		r1 = bitset<32>(value).to_ullong();
+
+		r1 = -((1 << value.length()) - r1);
+		return r1 + 1;
+	}
+	else
+	{
+		int result = 0;
+		for (int i = value.size(); i >= 0; i--)
+		{
+			if (value[i] == '1') {
+
+				result += pow(2, value.size() - i - 1);
+
+			}
+		}
+		return result;
+	}
+
+	return 0;
+}
 bool Memory::program_loc(string initial_loc, vector<string>instructions)
 {
 	Initial_loc = initial_loc;
@@ -105,9 +139,13 @@ string Memory::Iread(string address)
 
 void Memory::Iprint_memory()
 {
-	for (auto& it : memory)
+	/*for (auto& it : memory)
 	{
-		cout << it.first << "|" << it.second << endl;
+		cout << it.first << "|" << it.second << "(" endl;
+	}*/
+	for (auto& it : memory) {
+		int val = bin_decimal(it.second);
+		cout << it.first << "|" << setw(3) << it.second << "(" << val << ")" << "(" << decimal_hexa1(to_string(val)) << ")" << endl;
 	}
 }
 
